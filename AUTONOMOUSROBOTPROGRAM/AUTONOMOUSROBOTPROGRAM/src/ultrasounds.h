ec1fd84c39cd228925ff79499414098444892479
piranjamk <53086914+piranjamk@users.noreply.github.com>
2021-06-20 20:10:11 +0200

    Delete AUTONOMOUSROBOTPROGRAM/AUTONOMOUSROBOTPROGRAM directory

--- AUTONOMOUSROBOTPROGRAM/AUTONOMOUSROBOTPROGRAM/src/ultrasounds.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "stm32f1xx.h"
-
-void update_ultrasound_distances(float *distances_array);
-void update_ultrasound_front_distance(float *distances_array);
-void update_ultrasound_right_distance(float *distances_array);
